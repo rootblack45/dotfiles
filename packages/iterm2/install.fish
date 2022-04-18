20739cacd02222155f0e579ff05db6511d5eba89
Arongkorn <rootblack45@gmail.com>
2022-04-18 15:39:12 +0700

    refactor(pkg): add `kitty` config and remove `iterm2` config

--- packages/iterm2/install.fish
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env fish
-
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$DOTFILES_PKG/iterm2"
---
+++ packages/kitty/install.fish
@@ -0,0 +1,4 @@
+#!/usr/bin/env fish
+
+ln -sf $DOTFILES_PKG/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
+
