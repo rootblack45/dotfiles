b6df613d16058da260c6d5564ea1fe28dbcea9e9
Arongkorn <rootblack45@gmail.com>
2022-04-19 23:19:04 +0700

    refactor(pkg): use `!` instead of `not`

--- packages/fish/install.fish
+++ packages/fish/install.fish
@@ -1,10 +1,11 @@
 #!/usr/bin/env fish
 
-if not type -q omf
+if ! type -q omf
     curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 end
 
-if not omf list | grep -q boxfish
+if ! omf list | grep -q boxfish
     omf install boxfish
+    omf theme boxfish
 end
 
