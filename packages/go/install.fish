c1f6086e8ee7cd189c1f06a7bcbd9cc0e267b894
Arongkorn <rootblack45@gmail.com>
2022-04-19 23:10:39 +0700

    fix(pkg): ensure the folder exists before adding it to `PATH`

--- packages/go/install.fish
+++ packages/go/install.fish
@@ -1,5 +1,8 @@
 #!/usr/bin/env fish
 
+# ensure the go bin folder is exists
+mkdir -p (go env GOPATH)/bin
+
 if ! fish_add_path -m (go env GOPATH)/bin
     fish_add_path (go env GOPATH)/bin
 end
--- packages/rust/install.fish
+++ packages/rust/install.fish
@@ -1,5 +1,8 @@
 #!/usr/bin/env fish
 
+# ensure the cargo bin folder is exists
+mkdir -p $HOME/.cargo/bin
+
 if ! fish_add_path -m $HOME/.cargo/bin
     fish_add_path $HOME/.cargo/bin
 end
