3a23a0a6c2a73a0d3ef1162f27f876bd2de03b50
Arongkorn <rootblack45@gmail.com>
2022-06-24 02:51:54 +0700

    fix(pkg): source symlink path

--- packages/op/install.fish
+++ packages/op/install.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-ln -sf $DOTFILES_PKG/op/com.1password.SSH_AUTH_SOCK.plist $HOME/~/Library/LaunchAgents/com.1password.SSH_AUTH_SOCK.plist
+ln -sf $DOTFILES_PKG/op/com.1password.SSH_AUTH_SOCK.plist $HOME/Library/LaunchAgents/com.1password.SSH_AUTH_SOCK.plist
 
 if ! launchctl list | grep -q 'com.1password.SSH_AUTH_SOCK'
   launchctl load -w ~/Library/LaunchAgents/com.1password.SSH_AUTH_SOCK.plist
